6ad0cf74e5e67e64e0b31130c11f40c6ad458f27
Greg Gershman <greg@shelrick.com>
2013-01-23 20:06:45 -0500

    Move secret token to config file.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-MygovForms::Application.config.secret_token = 'b33d910f59861ef295e81ca0a4edca13b6bda10a71ff809079713b94a64c9ebfffe1baf8de72cb1b59ab161061cda4c3d396e0191995e8643b2ce5cf4862acbe'
+MygovForms::Application.config.secret_token = RAILS_SECRET_TOKEN
