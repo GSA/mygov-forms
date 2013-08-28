f71b70c23074381d24346b384d00b6498d6d9742
Justin Grevich <justin@grevi.ch>
2013-08-28 17:53:53 -0400

    [#55658046] add test for secure headers

--- spec/features/forms_spec.rb
+++ spec/features/forms_spec.rb
@@ -6,16 +6,17 @@ describe "Forms" do
         visit root_path
         page.should have_content "All Forms"
       end
+      
+      it "sets secure headers (X-Frame-Options & X-XSS-Protection)" do
+        # NOTE: the app also sets X-Content-Type-Options: nosniff, but that is only set for IE browsers
+        visit root_url
+
+        expect(page.response_headers["X-Frame-Options"]).to eq "SAMEORIGIN"
+        expect(page.response_headers["X-XSS-Protection"]).to eq "1; mode=block"
+      end
     
     context "when there are forms in the database" do
-      before do
-        @sample_form_1 = Form.create!(:title => 'Sample Form 1', :number => 'S-1')
-        sample_form_2 = Form.create!(:title => 'Sample Form 2', :number => 'S-2')
-        
-        @sample_form_1.form_fields.create!(:field_type => "string", :name => 'text_field', :label => 'A Text Field', :description => 'This is a text field.')
-        @sample_form_1.form_fields.create!(:field_type => "date_select", :name => 'date_field', :label => 'A Date Field', :description => 'This is a date field.')
-        @sample_form_1.form_fields.create!(:field_type => "select", :name => 'select_field', :label => 'A Select Field', :description => 'This is a select field', :options => [['Yes', 'Yes'], ['No', 'No']])
-      end
+      before {create_sample_forms}
       
       it "should list the forms on the home page and link to those forms by title and number" do
         visit root_path
@@ -29,7 +30,7 @@ describe "Forms" do
         page.should have_content "A Text Field"
         page.should have_content "A Date Field"
         page.should have_content "A Select Field"
-        fill_in 'data_text_field', :with => 'America'
+        fill_in 'data_text_field', with: 'America'
         click_button "Submit"
         page.should have_content "Your form has been submitted"
         page.should have_no_button "Download as PDF"
@@ -37,11 +38,39 @@ describe "Forms" do
         Submission.last.data[:select_field].should be_blank
       end
       
+      context "when a form has OMB data associated with it" do
+        it 'displays the OMB number and expiration date at the bottom of a form' do
+          visit root_path
+          click_link 'Sample Form 1 (S-1)'
+          expect(page).to have_content 'Form Approved OMB#4040-0001 | Expires at 2016-06-30'
+        end
+        
+        it 'provides a link to the information collection request / OIRA conclusion on Reginfo.gov' do
+          form = Form.find_by_title 'Sample Form 1'
+          
+          visit root_path
+          click_link 'Sample Form 1 (S-1)'
+          omb_link = find_link('Form Approved')
+
+          expect(omb_link[:href]).to have_content form.icr_reference_number
+        end
+        
+      end
+
+      context "when a form does not have OMB data associated with it" do
+        it 'does not display the omb info at the bottom of a form when the form does not have associated omb data' do
+          visit root_path
+          click_link 'Sample Form 2 (S-2)'
+          expect(page).to have_content 'Sample Form 2 (S-2)'
+          expect(page).to have_no_content 'Form Approved OMB#4040-0001 | Expires at 2016-06-30'
+        end
+      end
+      
       context "when the form has a PDF associated with it" do
         before do
-          pdf = @sample_form_1.build_pdf(:url => "http://example.gov/form.pdf")
+          pdf = @sample_form_1.build_pdf(url: "http://example.gov/form.pdf")
           pdf.save!
-          pdf_field = pdf.pdf_fields.create!(:name => 'text_field')
+          pdf_field = pdf.pdf_fields.create!(name: 'text_field')
           pdf_field.form_field = @sample_form_1.form_fields.first
           pdf_field.save!
         end
@@ -49,7 +78,7 @@ describe "Forms" do
         it "should allow the user to download a PDF version of their form if available" do
           visit root_path
           click_link 'Sample Form 1 (S-1)'
-          fill_in 'A Text Field', :with => 'America'
+          fill_in 'A Text Field', with: 'America'
           click_button "Submit"
           page.should have_link "Download as PDF"
         end
