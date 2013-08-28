2f4c3a2b843c70e1d6e7a80355d4ff4cd7332753
Justin Grevich <justin@grevi.ch>
2013-08-28 18:31:05 -0400

    [#55658046] add test for secure headers

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -21,5 +21,13 @@ describe "Home", :type => :request do
       visit root_path
       page.should have_content("Login")
     end
+    
+    it "sets secure headers (X-Frame-Options & X-XSS-Protection)" do
+      # NOTE: the app also sets X-Content-Type-Options: nosniff, but that is only set for IE browsers
+      visit root_path
+
+      expect(page.response_headers["X-Frame-Options"]).to eq "SAMEORIGIN"
+      expect(page.response_headers["X-XSS-Protection"]).to eq "1; mode=block"
+    end
   end
 end
