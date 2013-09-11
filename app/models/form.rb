3220b49202a4fddeed56de0b3d40ca5541ca4910
Raphael Villas <raphael.villas@gmail.com>
2013-09-11 14:39:55 -0400

    [#40148463] Made 'published_at' a required attribute and automatically set its value when nil

--- app/models/form.rb
+++ app/models/form.rb
@@ -2,10 +2,12 @@ class Form < ActiveRecord::Base
   has_many :form_fields, :order => '-position DESC, id ASC'
   has_one :pdf
   has_many :submissions
-  attr_accessible :number, :title, :form_fields_attributes, :icr_reference_number, :omb_control_number, :omb_expiration_date, :options, :start_content, :agency_name
-  validates_presence_of :number, :title, :start_content, :agency_name
+  attr_accessible :number, :title, :form_fields_attributes, :icr_reference_number, :omb_control_number, :omb_expiration_date, :options, :start_content, :agency_name, :published_at
+  validates_presence_of :number, :title, :start_content, :agency_name, :published_at
   validates_uniqueness_of :number
   accepts_nested_attributes_for :form_fields, :reject_if => :all_blank, :allow_destroy => true
+
+  before_validation :set_default_values
 
   def as_json(options = {})
     super(options.merge(:only => [:number, :title, :icr_reference_number, :omb_control_number, :omb_expiration_date]))
@@ -13,5 +15,9 @@ class Form < ActiveRecord::Base
 
   def to_param
     self.number.parameterize
+  end
+
+  def set_default_values
+    self.published_at = Time.now if published_at.nil?
   end
 end
